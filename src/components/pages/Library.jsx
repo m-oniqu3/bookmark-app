d1e1de71023ebc58908f0f289036e11b6e3509de
Monique McIntyre <skaiwilliams85@gmail.com>
2022-08-24 20:15:26 -0500

    Added library nav

--- src/components/pages/Library.jsx
+++ src/components/pages/Library.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
+import Container from "../helpers/container/Container";
+import LibraryNav from "../library/LibraryNav";
 
 const Library = () => {
-  return <div>Library</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  return (
+    <LibraryNav searchParams={searchParams} setSearchParams={setSearchParams} />
+  );
 };
 
 export default Library;
