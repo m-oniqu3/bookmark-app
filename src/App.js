ebd5d6a0ad738c2ad29612df668fa0a9f57a7fe6
Monique McIntyre <skaiwilliams85@gmail.com>
2022-08-30 19:17:09 -0500

    Added data to firebase

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import Pages from "./components/pages/Pages";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "./store/features/auth/authSlice";
 import { ReactNotifications } from "react-notifications-component";
 import "react-notifications-component/dist/theme.css";
+import { addDataToFirebase } from "./components/firebase/firebase-config";
 
 function App() {
   const dispatch = useDispatch();
+  const { user, isSignedIn } = useSelector((state) => state.auth);
+  const { library } = useSelector((state) => state.bookStore);
+  const { shelf } = useSelector((state) => state.bookShelf);
 
   // check if user data is stored in localStorage and update the store
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
     if (user) dispatch(setUser(user));
   }, [dispatch]);
+
+  //add library to firebase
+  useEffect(() => {
+    if (isSignedIn && (library.length > 0 || Object.keys(shelf).length > 0)) {
+      addDataToFirebase(user, library, shelf);
+    }
+  }, [isSignedIn, library, user, shelf]);
 
   return (
     <>
--- src/components/firebase/firebase-config.js
+++ src/components/firebase/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { collection, doc, getFirestore, setDoc } from "firebase/firestore";
 
 // firebase configuration
 const firebaseConfig = {
@@ -13,6 +14,21 @@ const firebaseConfig = {
 
 // initialize firebase
 const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
 
+// auth
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+
+// library
+export const libraryCollection = collection(database, "books");
+
+// add library to firebase
+export const addDataToFirebase = async (id, library, shelf) => {
+  const currentDoc = doc(database, "books", `${id}`);
+  await setDoc(
+    currentDoc,
+    { userData: { library: [...library], shelf: { ...shelf } } },
+    { merge: true }
+  );
+};
